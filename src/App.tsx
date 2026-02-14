1a4a8ef7a56345952cb38989c1715b3a0b77b9d2
saba-calin <sabaila.calin10@gmail.com>
2026-02-14 12:21:22 -0800

    Test endpoints

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,47 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios";
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const handleLogIn = async () => {
+    const response = await axios.post(
+      "https://api.story-point.xyz/log-in",
+      {
+        "firstName": "Calin",
+        "lastName": "Sabaila",
+        "username": "saba",
+        "email": "saba@gmail.com",
+        "password": "Roll.123"
+      },
+      {
+        withCredentials: true
+      }
+    );
+    console.log(response.data);
+  };
+
+  const handleCreateRoom = async () => {
+    const response = await axios.post(
+      "https://api.story-point.xyz/create-room",
+      {
+        "name": "roommm"
+      },
+      {
+        withCredentials: true
+      }
+    );
+    console.log(response.data);
+  }
+
+  const handleConnectToWs = () => {
+    const ws = new WebSocket("wss://ws.story-point.xyz");
+    ws.onopen = () => {
+      console.log("WebSocket connected");
+    };
+  }
 
   return (
     <>
@@ -21,6 +59,12 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <br />
+        <button onClick={handleLogIn}>Log In</button>
+        <br />
+        <button onClick={handleCreateRoom}>Create Room</button>
+        <br />
+        <button onClick={handleConnectToWs}>Connect to WebSocket</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
