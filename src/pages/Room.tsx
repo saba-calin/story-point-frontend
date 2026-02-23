901204c81a97c0fcf0b62d8dca647b3d32a99b5b
saba-calin <sabaila.calin10@gmail.com>
2026-02-23 14:52:18 -0800

    Test ws connectivity

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,4 +1,5 @@
 import {useNavigate, useParams} from "react-router-dom";
+import {useEffect} from "react";
 
 const Room = () => {
 
@@ -6,6 +7,16 @@ const Room = () => {
 
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const ws = new WebSocket(import.meta.env.VITE_WS_BASE_URL);
+    ws.onopen = () => {
+      console.log("WebSocket connected");
+      ws.send(JSON.stringify({
+        action: "join-room",
+        roomId: roomId
+      }));
+    };
+  }, []);
 
   return (
     <>
